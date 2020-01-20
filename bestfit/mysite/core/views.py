7a212f65ddf7a85277885cb559755214f34fff97
asad latif <asadlatif49@gmail.com>
2020-01-20 13:29:24 +0500

    1/20/2020

---
+++ bestfit/mysite/core/views.py
@@ -0,0 +1,82 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Test
+from django.contrib import messages
+from .forms import UserSignupForm
+
+
+def home(request):
+    count = User.objects.count()
+    return render(request, 'home.html', {
+        'count': count
+    })
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserSignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your Account has been created! your now able to login!')
+            return redirect('login')
+    else:
+        form = UserSignupForm()
+    return render(request, 'registration/signup.html', {
+        'form': form
+    })
+
+
+@login_required
+def secret_page(request):
+    return render(request, 'secret_page.html')
+
+
+class SecretPage(LoginRequiredMixin, TemplateView):
+    template_name = 'secret_page.html'
+
+
+def test_page(request):
+    questions= Test.objects.all()
+    return render(request,'General_knowledge.html', {"Test": questions})
+
+
+def result(request):
+    print("result page")
+    if request.method == 'POST':
+        data = request.POST
+        datas = dict(data)
+        qid = []
+        qans = []
+        ans = []
+        score = 0
+        for key in datas:
+            try:
+                qid.append(int(key))
+                qans.append(datas[key][0])
+            except:
+                print("Csrf")
+        for q in qid:
+            ans.append((Test.objects.get(id = q)).answer)
+        total = len(ans)
+        for i in range(total):
+            if ans[i] == qans[i]:
+                score += 1
+        # print(qid)
+        # print(qans)
+        # print(ans)
+        print(score)
+        eff = (score/total)*100
+    return render(request,
+        'result.html',
+        {'score':score,
+        'eff':eff,
+        'total':total})
+
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
